1b8af0021e084018a0e81f0b1c077b3e4ced600d
Mathieu Poussin <git@kedare.net>
2025-09-13 18:58:12 +0200

    fix ssh

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -5,9 +5,7 @@
     enable = true;
     permitRootLogin = "no";
     passwordAuthentication = false;
-    challengeResponseAuthentication = false;
     usePAM = true;
-    allowTcpForwarding = true;
     x11Forwarding = false;
     printMotd = false;
     clientAliveInterval = 300;
