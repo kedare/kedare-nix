ed2ea9a04299cac023c93e89233cbfc30abc4861
Mathieu Poussin <git@kedare.net>
2025-09-13 18:56:19 +0200

    Improve ssh security

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -1,5 +1,16 @@
 { ... }:
 
 {
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "no";
+    passwordAuthentication = false;
+    challengeResponseAuthentication = false;
+    usePAM = true;
+    allowTcpForwarding = true;
+    x11Forwarding = false;
+    printMotd = false;
+    clientAliveInterval = 300;
+    clientAliveCountMax = 2;
+  };
 }
