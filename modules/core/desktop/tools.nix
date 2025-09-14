1c5dfdd86cf8437381a85d71f506e9511313adac
Mathieu Poussin <git@kedare.net>
2025-09-14 15:38:52 +0200

    add fonts

--- modules/core/desktop/tools.nix
+++ modules/core/desktop/tools.nix
@@ -7,6 +7,23 @@
     vscode
   ];
 
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      fira-sans
+      nerd-fonts._0xproto
+      nerd-fonts.fira-code
+      nerd-fonts.droid-sans-mono
+    ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "0xProto Nerd Font"];
+        sansSerif = [ "Fira Sans" ];
+        serif = [ "DejaVu Serif" ];
+      };
+    };
+  };
+
   programs._1password.enable = true;
   programs._1password-gui = {
     enable = true;
