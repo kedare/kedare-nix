fb41d921737169a1a96a6339b7ec94adb8d34dd4
Mathieu Poussin <git@kedare.net>
2025-09-14 23:27:16 +0200

    add aliases

--- modules/core/global/shell.nix
+++ modules/core/global/shell.nix
@@ -4,6 +4,34 @@
 }:
 
 {
+  environment.shellAliases = {
+   y2mp3 = "youtube-dl -x --audio-format mp3 --xattrs --audio-quality 7 --add-metadata --embed-thumbnail";
+   gco = "git commit";
+   gps = "git push";
+   gpl = "git pull";
+   gs = "git status";
+   glg = "git log";
+   ga = "git add";
+   gaa = "git add -A";
+   gcl = "git clone";
+   gd = "git diff";
+   gdc = "git diff --cached";
+   gcs = "git clone --depth=1";
+   gcam = "git commit -am";
+   gchm = "git checkout \$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')";
+   gca = "git add -A";
+   gcb = "git checkout -b";
+   ggc = "git gc --force --aggressive";
+   e = "emacsclient -t";
+   edm = "emacs --daemon";
+   vim = "lvim";
+   aras = "ara-manage runserver";
+   tf = "terraform";
+   cat = "bat";
+   wt = "watch -n1 $(fc -ln -1)";
+   k = "kubectl";
+   lsx = "exa -lha --git";
+  };
 
   i18n.defaultLocale = "en_US.UTF-8";
 
