9915e89447030bb4e9d2e5a444ff704c84063bb7
Mathieu Poussin <git@kedare.net>
2025-09-19 12:32:56 +0200

    fix carbuncle hostname

--- hosts/carbuncle/configuration.nix
+++ hosts/carbuncle/configuration.nix
@@ -9,7 +9,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "garuda";
+  networking.hostName = "carbuncle";
   time.timeZone = "Europe/Madrid";
 
   boot.initrd.luks.devices = {
