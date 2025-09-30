0e93061741065047075548bb60963a15fea535f0
Mathieu Poussin <git@kedare.net>
2025-09-30 10:13:33 +0200

    syntax

--- modules/core/global/security.nix
+++ modules/core/global/security.nix
@@ -5,9 +5,9 @@
     enable = true;
     maxretry = 10;
     ignoreIP = [
-      192.168.88.0/22
-      2001:67c:da8:1000::/60
-    ];
+      "192.168.88.0/22"
+      "2001:67c:da8:1000::/60"
+      ];
     jails = {
       sshd = {
         enabled = true;
